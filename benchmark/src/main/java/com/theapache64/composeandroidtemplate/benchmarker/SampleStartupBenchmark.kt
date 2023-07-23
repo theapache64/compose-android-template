8751d2f1c2bcf19c4c67be972639ab170144cf50
theapache64 <theapache64@gmail.com>
2023-07-23 14:25:15 +0530

    🌟 add benchmark module

---
+++ benchmark/src/main/java/com/theapache64/composeandroidtemplate/benchmarker/SampleStartupBenchmark.kt
@@ -0,0 +1,30 @@
+package com.theapache64.composeandroidtemplate.benchmarker
+
+import androidx.benchmark.macro.StartupTimingMetric
+import androidx.benchmark.macro.junit4.MacrobenchmarkRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class SampleStartupBenchmark {
+    @get:Rule
+    val benchmarkRule = MacrobenchmarkRule()
+
+    @Test
+    fun startup() = benchmarkRule.measureRepeated(
+        packageName = "com.theapache64.composeandroidtemplate",
+        metrics = listOf(StartupTimingMetric()),
+        iterations = 5,
+        setupBlock = {
+            // Press home button before each run to ensure the starting activity isn't visible.
+            pressHome()
+        }
+    ) {
+        // starts default launch activity
+        startActivityAndWait()
+    }
+}
